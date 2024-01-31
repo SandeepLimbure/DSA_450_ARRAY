088ddfb5cf705e0a2d778c22a97565ba93efd2f7
sandeep limbure <127886236+SandeepLimbure@users.noreply.github.com>
2024-02-01 00:10:13 +0530

    Add files via upload

---
+++ DuplicateValue.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class DuplicateValue {
+    public static void main(String[] args) {
+        int []arr = {1,3,4,2,2};
+        System.out.println(Value(arr));
+    }
+    static int Value(int []arr){
+        for(int i = 0;i<arr.length;i++){
+            for(int j = i+1;j< arr.length;j++){
+                if(arr[i]==arr[j]){
+                    return arr[i];
+                }
+            }
+        }
+        return -1;
+    }
+}
+
